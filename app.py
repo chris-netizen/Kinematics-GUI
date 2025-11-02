28041b91c88feb4ceec0f74326e4d0d722681930
chris-netizen <adedejisomoye1@gmail.com>
2025-11-02 03:39:29 +0100

    remove unused dependencies in app.py file

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import sys
 from gui import MainWindow
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout,
-                             QHBoxLayout, QPushButton, QLineEdit, QLabel, QTableView,
-                             QTextEdit, QGraphicsView, QGraphicsScene, QMessageBox,
-                             QMenuBar, QAction, QStatusBar, QFileDialog, QComboBox)
+from PyQt5.QtWidgets import QApplication
 
 
 if __name__ == "__main__":
